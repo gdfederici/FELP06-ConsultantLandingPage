a14d6f4bd69137b7dfe9bc38457c921c211a2c85
gdfederici <gdfederici@gmail.com>
2021-04-27 11:18:42 +0200

    Netlify: test for problem during deploy.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: "production", 
     // IT_ Guarda i file e ricompila ogni volta che cambiano.
     // EN_ Watch files and recompile whenever they change.
-    watch: true,
+    //watch: true,
     // IT_ File Javascript di entrata pre-compilato.
     // EN_ Pre-compiled entry Javascript file.
     entry: {
