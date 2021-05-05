3d4cb7301a0e4f673fb102196eb2d23a4197f8ad
gdfederici <gdfederici@gmail.com>
2021-05-05 17:10:40 +0200

    JS - Change some for cycle with forEach.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,24 +3,26 @@ import Thumb from '../img/consultantlandingpage_thumb.jpg';
 
 /*** ------------------------- *** ------------------------- *** ------------------------- *** ------------------------- ***/
 
+/*** HAMBURGER MENU ***/
 window.magicMenu = function() {
     document.getElementById("hamburgerNav").classList.toggle("responsive"); //IT_ Aggiunge/rimuove la classe per la visualizzazione del menu. | EN_ Add/remove the class for the menu display.
     var elMenuCta = document.getElementById("hamburgerCTA");
     (elMenuCta.classList.contains("button")) ? (elMenuCta.classList.remove("button", "button__base")) : (elMenuCta.classList.add("button", "button__base")); // IT_ Aggiunge/rimuove la classe bottone a elemento CTA, così appare semplice in mobile e bottone in desktop. | EN_ Add/remove the button class to the CTA element.
 }
 
+/*** BUTTON LOAD MORE FAQ ***/
 window.moreFaq = function() {
-    var faqExtra = document.getElementsByClassName("faq-extra");
-    for ( let i = 0; i < faqExtra.length; i++) {
-        faqExtra[i].style.display = "flex";
-    }
-    var faqMobile = document.getElementsByClassName("faq__mobile");
-    for ( let i = 0; i < faqMobile.length; i++) {
-        faqMobile[i].style.display = "flex";
-    }
+    /* var faqExtra = document.getElementsByClassName("faq-extra");
+       for ( let i = 0; i < faqExtra.length; i++) { faqExtra[i].style.display = "flex"; } */
+    /* IT_ Collezione di oggetti simil vettore, ma non vettore per questo forEach non funziona. Per farlo funzionare occorre aggiungere come di seguito - VALE PER TUTTI */
+    Array.from(document.getElementsByClassName("faq-extra")).forEach(element => element.style.display = "flex" );
+    /*  var faqMobile = document.getElementsByClassName("faq__mobile");
+        for ( let i = 0; i < faqMobile.length; i++) { faqMobile[i].style.display = "flex"; } */
+    Array.from(document.getElementsByClassName("faq__mobile")).forEach(element => element.style.display = "flex" );
     document.getElementById("more-faq").style.display = "none";
 }
 
+/*** FORM VALIDATION ***/
 window.validateForm = function() {
     let usrRegex = /^[A-Za-z\d]+$/;
     let pswRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&].{8,}$/;
@@ -61,8 +63,7 @@ window.moreTestimonial = function() {
 function showTestimonial(n) {
     var singleTestimonial = document.getElementsByClassName("testimonial-single");
     if (n > singleTestimonial.length) {slideTestimonial = 1}
-    for (let j = 0; j < singleTestimonial.length; j++) {
-        singleTestimonial[j].style.display = "none";
-    }
+    /* for (let j = 0; j < singleTestimonial.length; j++) { singleTestimonial[j].style.display = "none"; } */
+    Array.from(singleTestimonial).forEach(element => element.style.display = "none" );
     singleTestimonial[slideTestimonial-1].style.display = "block";
 }
